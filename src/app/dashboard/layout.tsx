8ed765540f649f49c9787a79decad337df3a0dd9
Ayrton Silas <ayrton.silas@jusbrasil.com.br>
2025-04-05 22:22:42 -0300

    first

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,1 +1,12 @@
- 
+'use client'
+
+import SidebarLayout from '@/components/SidebarLayout'
+import React from 'react'
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <SidebarLayout>{children}</SidebarLayout>
+} 
